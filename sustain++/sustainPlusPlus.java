b1203ee4b707cd3369cbf87ba9b46882a71ce30c
Mario Lubenka <mario.lubenka@googlemail.com>
2020-02-13 18:46:12 +0100

    Create sustainPlusPlus.java

---
+++ sustain++/sustainPlusPlus.java
@@ -0,0 +1,15 @@
+package extraLarge;
+
+/**
+* The goal of this program is to obtain a HEALTHY
+* and SUSTAINABLE relationship,darling.
+* 
+* @author Cassie Wei from Mili
+*/
+public class sustainPlusPlus {
+  public static void main(String[] args) {
+    World world = new World();
+    Life me = new Ghost();
+    Life you = new Ghost();
+  }
+}
